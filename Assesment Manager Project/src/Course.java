d2d6637ed166326b4528ded79a8b8f11744b5ac7
JulienGorjon <julien.gorjon@gmail.com>
2019-11-07 11:58:35 +0100

    Student class and Course modified

--- Assesment Manager Project/src/Course.java
+++ Assesment Manager Project/src/Course.java
@@ -1,13 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 
 public class Course {
 	public String name;
 	public String code;
 	public int creditNumber;
-	// list of students that follow the course
+	public List<Student> studentList;
+	Dictionary<Integer, Dictionary> years_points_list = new Hashtable<Integer, Dictionary>();
+	Dictionary<Integer, Integer> points_list2019 = new Hashtable<Integer, Integer>();
+
+
 	
 	public Course(String courseName,String courseCode,int creditNumber) {
 		this.name=courseName;
 		this.code=courseCode;
 		this.creditNumber= creditNumber;
+		studentList=new ArrayList<Student>();
+		
+		points_list2019.put(16119,15);
+		points_list2019.put(16150,16);
+		years_points_list.put(2019, points_list2019);
 	}
 }
---
+++ Assesment Manager Project/src/Student.java
@@ -0,0 +1,11 @@
+
+public class Student {
+	public String name;
+	public int ID_number;
+	
+	public Student(String name, int ID) {
+		this.name=name;
+		this.ID_number=ID;
+	}
+
+}
