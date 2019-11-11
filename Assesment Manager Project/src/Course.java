a27bf21e71b312d0a1e8f27b9ff4cfdfc825f5e4
JulienGorjon <julien.gorjon@gmail.com>
2019-11-11 12:48:51 +0100

    Nested dictionnary and method to add points

--- Assesment Manager Project/src/Course.java
+++ Assesment Manager Project/src/Course.java
@@ -1,17 +1,20 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Dictionary;
-import java.util.Hashtable;
+//import java.util.Dictionary;
+//import java.util.Hashtable;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Course {
 	public String name;
 	public String code;
+	public int currentYear=2019;
 	public int creditNumber;
 	public List<Student> studentList;
-	Dictionary<Integer, Dictionary> years_points_list = new Hashtable<Integer, Dictionary>();
-	Dictionary<Integer, Integer> points_list2019 = new Hashtable<Integer, Integer>();
-
+	//Dictionary<Integer, Dictionary> years_points_dico = new Hashtable<Integer, Dictionary>();
+	//Dictionary<Integer, Float> points2019_dico = new Hashtable<Integer, Float>();
+	Map<Integer,HashMap<Integer,Float>>pointsDico;
 
 	
 	public Course(String courseName,String courseCode,int creditNumber) {
@@ -19,9 +22,20 @@ public class Course {
 		this.code=courseCode;
 		this.creditNumber= creditNumber;
 		studentList=new ArrayList<Student>();
+		pointsDico=new HashMap<Integer,HashMap<Integer,Float>>();
 		
-		points_list2019.put(16119,15);
-		points_list2019.put(16150,16);
-		years_points_list.put(2019, points_list2019);
+		//points2019_dico.put(16119,(float)15.0);
+		//points2019_dico.put(16150,(float)16.0);
+		//years_points_dico.put(2019, points2019_dico);
 	}
+	
+	public void addStudent(Student stud) {
+		studentList.add(stud);
+	}
+	
+	public void addPoints(int id, float points) {
+		pointsDico.get(currentYear).put(id, points);
+		//pointsDico.put(2019,new HashMap(<Integer,Float> ){{put((int)16119,(float)16);}});
+	}
+	
 }
--- Assesment Manager Project/src/Teacher.java
+++ Assesment Manager Project/src/Teacher.java
@@ -1,32 +1,44 @@
 import java.util.ArrayList;
+//import java.util.Dictionary;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Teacher{
 	public String name;
 	public int ID_number;
 	private String passwd;
-	public List<Course> courseList;
+	//public List<Course> courseList;
+	Map<String,Course>coursesDico; 
 	
-	// list of students that follow the course
+
 	
 	public Teacher(String name,int ID,String passwd) {
 		this.name= name;
 		this.ID_number= ID;
 		this.passwd= passwd;
-		courseList=new ArrayList<Course>();
+		coursesDico=new HashMap<String,Course>();
+		//courseList=new ArrayList<Course>();
 		
 	}
 	
 	public void addCourse(Course course) {
-		courseList.add(course);
+
+		coursesDico.put(course.code,course);
+		//courseList.add(course);
 	}
 	
 	public List<Course> getCourseList(){
-		return this.courseList;
+		ArrayList<Course> coursesList = new ArrayList<>(coursesDico.values());
+		return coursesList;
 	}
 	
 	
 	public boolean checkPasswd(String passwd) {
 		return (this.passwd==passwd);
+	}
+	
+	public void encodePoints(String courseCode, int id, float points) {
+		coursesDico.get(courseCode).addPoints(id,points);
 	}
 }
